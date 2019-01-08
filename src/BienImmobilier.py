7bfc39d0bc5243f6c03c75e10059e960345c0b1c
vvv <vincent.cizey@orange.fr>
2019-01-08 17:03:11 +0100

    sauvegarde de la nuit

--- src/BienImmobilier.py
+++ src/BienImmobilier.py
@@ -19,6 +19,36 @@ Methodes :
 -
 """
 
-class BienImmobilier(self):
-    pass
+class BienImmobilier:
 
+    num = 1
+
+    def __init__(self, adresse, orientation, prixDemande, dateVenteSouhait, dateDispo, nombrePieces):
+        self.id = self.num
+        self.adresse
+        self.orientation
+        self.prixDemande
+        self.dateVenteSouhait
+        self.dateDispo
+        self.nombrePieces
+        self.num += 1
+
+    """
+    def AjoutBien(self, adresse, orientation, prixDemande, dateVenteSouhait, dateDispo, nombre, nombrePieces):
+
+    complet = False
+    while complet == False:
+        adresse = input("entrer l'adresse du bien : ")
+        adresse = input("entrer l'orientation du bien : ")
+        adresse = input("entrer le prix demandé du bien : ")
+        adresse = input("entrer la date de vente souhaité : ")
+        adresse = input("entrer la date de disponnibilité du bien : ")
+        adresse = input("entrer le nombre de pièces du bien : ")
+        
+        # vérifier la conformité des infos
+        
+        complet = True
+
+        """
+
+    hh
