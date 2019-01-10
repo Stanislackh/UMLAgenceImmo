731297a4f1cc3c7215c141df1f89479e0485e91f
Stanislackh <kohler.stanislas@gmail.com>
2019-01-10 16:52:29 +0100

    Classe Personne bien avancée
    2 methodes youhou

--- src/Personnes.py
+++ src/Personnes.py
@@ -7,40 +7,51 @@ Attributs :
 - Telephone
 - Mail
 Methodes :
-- AjoutClient()
--
+- ajoutClient()
+- afficherInfosClient()
 -
 -
 -
 """
-
+import re
 
 class Personne:
+
     # Initialise l'incrémentation des personnes
     identifiant = 0
+
     # Initialisation de la liste de clients
     clients = {}
 
     # Initialisation de la classe avec les 5 Attibuts
-    def __init__(self, pfnom, pfprenom, pftelephone, pfmail):
-        self.nom = pfnom
-        self.prenom = pfprenom
-        self.telephone = pftelephone
-        self.mail = pfmail
-        Personne.identifiant = 1 + Personne.identifiant
+    def __init__(self, pfnom = "", pfprenom = "", pfadresse = "",pftelephone = 0, pfmail = ""):
 
-    def AjoutDico(self):
-        self.clients[self.identifiant] = self.nom + " " + self.prenom + " " + self.telephone + " " + self.mail
+      self.nom = pfnom
+      self.prenom = pfprenom
+      self.adresse = pfadresse
+      self.telephone = pftelephone
+      self.mail = pfmail
 
+    #Permet d'ajouter un nouveau client
+    def ajouterClient(self):
 
-"""TESTS"""
+        #Entrée des données par l'utilisateur
+        self.nom = input("Entrez le nom du nouveau client : ")
+        self.prenom = input("Entrez le prénom du nouveau client : ")
+        self.adresse = "pas d'adresse"
+        self.telephone = str(input("Entrez le téléphone du nouveau client : "))
+        self.mail = input("Entrez le mail du nouveau client : ")
 
-p1 = Personne("joe", "trede", "0241512131", "joe@trede")
-# print(p1.nom)
-p1.AjoutDico()
-# print(p1.clients)
+        #Incrémentation du nombre de clients de 1
+        Personne.identifiant = Personne.identifiant + 1
 
-p2 = Personne("joyooode", "trede", "0241512131", "joe@trede")
-p2.AjoutDico()
+        #Ajoute les élements dans le dictionnaire
+        self.clients[self.identifiant] = Personne(self.nom, self.prenom,self.adresse,self.telephone,self.mail)
 
-print(Personne.clients)
+    #Permet d'afficher les infos pour le client selectionné
+    def afficherInfosClient(self, IdPersonne):
+        print(Personne.clients[IdPersonne])
+
+    #Permet d'afficher les informations de l'objet
+    def __str__(self):
+      return "Infos du client => Id Client : {}; Nom : {}; Prenom : {}; Téléphone : {}; Adresse : {}; Mail : {}".format(self.identifiant, self.nom, self.prenom, self.telephone, self.adresse, self.mail)
