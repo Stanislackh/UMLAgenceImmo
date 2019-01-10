031a574760833660b671912b713118833f5ce38a
vvv <vincent.cizey@orange.fr>
2019-01-10 17:56:02 +0100

    commit de fin de journée

--- src/BienImmobilier.py
+++ src/BienImmobilier.py
@@ -1,54 +1,81 @@
-"""Définition de la classe de BienImmobilier avec les attributs suivants:
-    - id()
-    ect...
 
-    """
-
-
-"""Classe Bien immobilier 
-Attributs :
--
--
--
--
-Methodes :
--
--
--
--
--
-"""
 
 class BienImmobilier:
 
+    #identifiant unique incrémenté à chaque instanciation
     num = 1
 
-    def __init__(self, adresse, orientation, prixDemande, dateVenteSouhait, dateDispo, nombrePieces):
-        self.id = self.num
-        self.adresse
-        self.orientation
-        self.prixDemande
-        self.dateVenteSouhait
-        self.dateDispo
-        self.nombrePieces
-        self.num += 1
+    #Liste des biens inscrits
+    listeBien = []
 
-    """
-    def AjoutBien(self, adresse, orientation, prixDemande, dateVenteSouhait, dateDispo, nombre, nombrePieces):
+    def __init__(self):
+        self.num = 0
+        self.adresse = ""
+        self.orientation = ""
+        self.prixDemande = 0
+        self.dateVenteSouhait = ""
+        self.dateDispo = ""
+        self.nombrePieces = 0
 
-    complet = False
-    while complet == False:
-        adresse = input("entrer l'adresse du bien : ")
-        adresse = input("entrer l'orientation du bien : ")
-        adresse = input("entrer le prix demandé du bien : ")
-        adresse = input("entrer la date de vente souhaité : ")
-        adresse = input("entrer la date de disponnibilité du bien : ")
-        adresse = input("entrer le nombre de pièces du bien : ")
-        
-        # vérifier la conformité des infos
-        
-        complet = True
+    def __str__(self):
+        return "" + self.adresse
 
-        """
+    def __repr__(self):
+        return "" + self.adresse
 
-    hh
+    def inscrire(self, adresse, orientation, prixDemande, dateVenteSouhait, dateDispo, nombrePieces):
+
+        #complet deviandra True quand les données seront validées, permet de boucler avant ça
+        complet = False
+
+
+        erreur = False
+
+        while not complet:
+
+            if erreur:
+                print("les informations sont incorrectes, veullez réessayer SVP")
+
+                # A FAIRE : vérifier la conformité des infos
+
+            # complet passe à True si les données sont validées
+            complet = True
+            # erreur passe à True
+            erreur = True
+
+        # on incrémente la varible de classe identifiant et on l'affecte à l'identifiant de l'objet
+        self.num = BienImmobilier.num
+        BienImmobilier.num = BienImmobilier.num + 1
+
+        self.adresse = adresse
+        self.orientation = orientation
+        self.prixDemande = prixDemande
+        self.dateVenteSouhait = dateVenteSouhait
+        self.dateDispo = dateDispo
+        self.nombrePieces = nombrePieces
+
+        BienImmobilier.listeBien.append(self)
+
+    def afficherTousBiens_adresse(self):
+        return BienImmobilier()
+
+
+"""TESTs"""
+
+b1 = BienImmobilier()
+b1.inscrire("loin", "de gauche", "trop", "vite", "maintenant", 1)
+print(b1.listeBien)
+print(b1.num)
+
+b2 = BienImmobilier()
+b2.inscrire("fffffff", "ffffff", "ffff", "fffffff", "ffffff", 2)
+print(b2.listeBien)
+print(b2.num)
+
+b3 = BienImmobilier()
+b3.inscrire("zzzzzzzz", "zzzzzz zzzzzzzz", "zzzzzzz", "zzzzzzzz", "zzzzzzzzz", 1234567)
+print(b3.listeBien)
+print(b3.num)
+
+print(b3)
+print(BienImmobilier.listeBien[0].orientation + "  res")
