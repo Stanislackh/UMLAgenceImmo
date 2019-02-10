d673d9b56d1a6685348ab81bf6c67882544eee45
Slackh <kohler.stanislas@gmail.com>
2019-02-10 15:23:30 +0100

    Promesse de ventes ok reste a faire les conditions de refus

--- src/voeux.py
+++ src/voeux.py
@@ -1,14 +1,18 @@
 """Classe Voeux"""
 
 from personnes import Personnes
-from agence import Agence
+import agence
 from mendat import Mendat
+import rendezVous
 
 class Voeux:
 
     #Dictionnaire liste Voeux
     listeVoeux = {}
     keyL = 0
+
+    #Liste des informations client/bien
+    infos = []
 
     def __init__(self,pfprix = 0, pftypebien = "res"):
         self.bienSouhaite = pftypebien
@@ -45,12 +49,12 @@ class Voeux:
         #Compliqué AF
 
         #les voeux
-
+        infos = []
         acheteur = ""
         vendeur = ""
 
         #Parcours de la liste de bien
-        for i in Agence.listeBien.values():
+        for i in agence.Agence.listeBien.values():
             vendeur = i[1][0]
 
             #Parcours de la liste de voeux par bien proposé
@@ -70,13 +74,16 @@ class Voeux:
                         #prix souhaité              Surface souhaité          nombmre de pièces
                         if (i[1][1][1] == j[2]) and (i[1][1][6] == j[3]) and (i[1][1][4] == j[4]):
                             print("Le bien correspond aux recherches")
-                            #Autorise le bien a être visité
                             Mendat.autorisation(self)
-                            #Renvoi les coordonnées du l'acheteur du vendeur et les infos sur le bien du vendeur
-                            return j[1][2], i[0][0][2], i[1][1]
+                            #Ajoute les coordonnées du l'acheteur du vendeur et les infos sur le bien du vendeur
+                            Voeux.infos.append(j[1][2])
+                            Voeux.infos.append(i[0][0][2])
+                            Voeux.infos.append(i[1][1])
+                            rendezVous.RendezVous.confirmerVente(self)
+                            return Voeux.infos
+
                         else:
                             print("un critère ne correspond pas")
-
 
                 #Cas de recherche de terrain
                 if (vendeur == "terrain") and (acheteur == "terrain"):
@@ -87,10 +94,12 @@ class Voeux:
                         #prix souhaité              Surface souhaité
                         if (i[1][1][1] == j[2]) and (i[1][1][4] == j[3]):
                             print("Le bien correspond aux recherches")
-                            # Autorise le bien a être visité
-                            Mendat.autorisation(self)
-                            # Renvoi les coordonnées du l'acheteur du vendeur et les infos sur le bien du vendeur
-                            return j[1][2], i[0][0][2], i[1][1]
+                            #Ajoute les coordonnées du l'acheteur du vendeur et les infos sur le bien du vendeur
+                            infos.append(j[1][2])
+                            infos.append(i[0][0][2])
+                            infos.append(i[1][1])
+                            return Voeux.infos
+
                         else:
                             print("un critère ne correspond pas")
 
@@ -102,16 +111,19 @@ class Voeux:
                         # prix souhaité              Nombre de pièces souhaité
                         if (i[1][1][1] == j[2]) and (i[1][1][4] == j[3]):
                             print("Le bien correspond aux recherches")
-                            # Autorise le bien a être visité
-                            Mendat.autorisation(self)
-                            # Renvoi les coordonnées du l'acheteur du vendeur et les infos sur le bien du vendeur
-                            return j[1][2], i[0][0][2], i[1][1]
+                            #Ajoute les coordonnées du l'acheteur du vendeur et les infos sur le bien du vendeur
+                            infos.append(j[1][2])
+                            infos.append(i[0][0][2])
+                            infos.append(i[1][1])
+                            print(Voeux.infos)
+                            return Voeux.infos
+
                         else:
                             print("Un critère ne convient pas")
 
 
 if __name__ == "__main__":
-    a = Agence()
+    a = agence.Agence()
     a.inscriptionVendeur()
     b = Voeux()
     b.voeuxClient()
