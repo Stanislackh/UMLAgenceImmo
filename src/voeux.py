e3e4fde52349272b8208710c13f93fac9e19f738
Stan <kohler.stanislas@gmail.com>
2019-02-10 09:50:51 +0100

    avancée voeux

--- src/voeux.py
+++ src/voeux.py
@@ -23,21 +23,23 @@ class Voeux:
             type = input("Type de bien, tapez le type de bien a inscrire : appartement, maison ou terrain : ")
 
         if type.lower() == "maison":
-
+            self.prixSouhaite = input("Prix souhaité pour le bien : ")
             self.surface = input("Surface au sol souhaité : ")
             self.nbPieces = input("Nombre de pièces souhaité :")
 
-            Voeux.listeVoeux[Voeux.keyL] = type, Personnes.ajouterClient(self), self.surface, self.nbPieces
+            Voeux.listeVoeux[Voeux.keyL] = type, Personnes.ajouterClient(self), self.prixSouhaite, self.surface, self.nbPieces
 
         elif type.lower() == "terrain":
+            self.prixSouhaite = input("Prix souhaité pour le bien : ")
             self.surface = input("Surface au sol souhaité : ")
 
-            Voeux.listeVoeux[Voeux.keyL] = type, Personnes.ajouterClient(self), self.surface
+            Voeux.listeVoeux[Voeux.keyL] = type, Personnes.ajouterClient(self), self.prixSouhaite, self.surface
 
         elif type.lower() == "appartement":
+            self.prixSouhaite = input("Prix souhaité pour le bien : ")
             self.nbPieces = input("Nombre de pièces souhaité :")
 
-            Voeux.listeVoeux[Voeux.keyL] = type, Personnes.ajouterClient(self), self.nbPieces
+            Voeux.listeVoeux[Voeux.keyL] = type, Personnes.ajouterClient(self), self.prixSouhaite, self.nbPieces
 
     def checkVoeux(self):
         #Compliqué AF
@@ -63,22 +65,28 @@ class Voeux:
                         if i[1][1][1] == j[2]:
                             print("Prix ok")
                         #nombrede pièces souhaité
-                        if i[1][1][4] == j[3]:
+                        if i[1][1][4] == j[4]:
                             print("nombre pièces ok")
                             print("Mise en relation")
+                        return j[1][2], i[0][0][2]
 
                 #Cas de recherche de terrain
                 if (vendeur == "terrain") and (acheteur == "terrain"):
                     print("TERRAIN")
+                    # Informations client vendeur
+                    # print(i[0][0][2])
+                    # Informations client acheteur
+                    # print(j[1][2])
                     #vérification pour un terrain
                     if vendeur == acheteur:
                         #prix souhaité
                         if i[1][1][1] == j[2]:
                             print("Prix ok")
                         #nombrede pièces souhaité
-                        if i[1][1][4] == j[2]:
+                        if i[1][1][4] == j[3]:
                             print("surface ok")
                             print("Mise en relation")
+                            return j[1][2], i[0][0][2]
 
                 # Cas de recherche d' appartement
                 if (vendeur == "appartement") and (acheteur == "appartement"):
@@ -89,14 +97,14 @@ class Voeux:
                         if i[1][1][1] == j[2]:
                             print("Prix ok")
                         # nombrede pièces souhaité
-                        if i[1][1][4] == j[2]:
+                        if i[1][1][4] == j[4]:
                             print("nb Pièces ok")
                             print("Mise en relation")
-
+                            return j[1][2], i[0][0][2]
 
     def infoMendat(self):
         #Donne les information sur le bien et fait visiter
-        pass
+        return Voeux.checkVoeux(self)
 
 if __name__ == "__main__":
     a = Agence()
