ff0d1b6ee62d6f2ced9ec0e3c99680a233d6c2a3
vincent <yizick@gmail.com>
2019-02-10 11:00:36 +0100

    Auto stash before merge of "vincent" and "dev"

--- src/Agence.py
+++ src/Agence.py
@@ -1,12 +1,10 @@
 """Création de l'agence"""
 
-#from BienImmobilier import BienImmobilier
-from Personnes import Personnes
-from Morale import Morale
-import Mendat
+from personnes import Personnes
+from morale import Morale
 from BI import BI
 import TB
-from rendezVous import RendezVous
+import rendezVous
 
 class Agence:
 
@@ -31,62 +29,55 @@ class Agence:
             #Vendeur Physique
             if type.lower() == "physique":
 
-                #Incrémente la clé vendeur
+
+                #Incrémente les clés vendeur et bien
                 Agence.keyV += 1
+
                 #Ajoute le vendeur au dictionnaire et ajoute la durée du mendat
-                Agence.vendeur[Agence.keyV] = Personnes.ajouterClient(self), Personnes.ajouterMendat(self)
-                Agence.listeBien[Agence.keyB] = Agence.vendeur[Agence.keyV], RendezVous.prendreRDV(self)
+                Agence.vendeur[Agence.keyV] =  Personnes.ajouterClient(self), Personnes.ajouterMendat(self)
+                Agence.listeBien[Agence.keyB] = Agence.vendeur[Agence.keyV], rendezVous.RendezVous.prendreRDV(self)
+
 
             #Vendeur Morale
             elif type.lower() == "morale":
 
-                #Incrémente la clé vendeur
+                # Incrémente les clés vendeur et bien
                 Agence.keyV += 1
                 #Ajoute le vendeur au dicotionnaire et ajoute la durée du mendat
-                Agence.vendeur[Agence.keyV] = Morale.ajouterPersonneMorale(self)
+                Agence.vendeur[Agence.keyV] = type, Morale.ajouterPersonneMorale(self)
+                Agence.listeBien[Agence.keyB] = Agence.vendeur[Agence.keyV], rendezVous.RendezVous.prendreRDV(self)
 
-                Agence.listeBien[Agence.keyB] = Agence.vendeur[Agence.keyV], RendezVous.prendreRDV(self)
 
     #Initialise la liste des annonces
     annonce = {}
 
-    #Initialise le compteur de bien
-    compt = 0
-
-    #Inscrit un Acheteur potentiel
-    def inscriptionAcheteur(self, type = "r"):
-
-        #Incrémente les acheteur
-        Agence.keyA += 1
-
-        while (type != "physique") and (type != "morale"):
-            type = input("Tapez physique pour inscrire une personne ou morale pour une entreprise : ")
-
-            #Vendeur Physique
-            if type.lower() == "physique":
-                #Ajoute le vendeur au dictionnaire
-                Agence.acheteur[Agence.keyA] = Personnes.ajouterClient(self)
-
-            #Vendeur Morale
-            elif type.lower() == "morale":
-                #Ajoute le vendeur au dicotionnaire
-                Agence.acheteur[Agence.keyA] = Morale.ajouterPersonneMorale(self)
-
-    #Rentre les information lors du rendez vous
-    # def encoursRDV(self):
-    #     Agence.annonce[Agence.compt] = BienImmobilier.inscrire(self,"loin", "de gauche", "trop", "vite", "maintenant", 1)
-    #     Agence.compt = Agence.compt + 1
-    #     print(Agence.annonce)
-
+    # #Inscrit un Acheteur potentiel
+    # def inscriptionAcheteur(self, type = "r"):
+    #
+    #     #Incrémente les acheteur
+    #     Agence.keyA += 1
+    #
+    #     while (type != "physique") and (type != "morale"):
+    #         type = input("Tapez physique pour inscrire une personne ou morale pour une entreprise : ")
+    #
+    #         #Vendeur Physique
+    #         if type.lower() == "physique":
+    #             #Ajoute le vendeur au dictionnaire
+    #             Agence.acheteur[Agence.keyA] = Personnes.ajouterClient(self)
+    #
+    #         #Vendeur Morale
+    #         elif type.lower() == "morale":
+    #             #Ajoute le vendeur au dicotionnaire
+    #             Agence.acheteur[Agence.keyA] = Morale.ajouterPersonneMorale(self)
 
     def __str__(self):
         return str(Agence.vendeur)
 
 
 """Test"""
+if __name__ == "__main__":
+    a = Agence()
+    a.inscriptionVendeur()
+    print(a.vendeur)
+    print(a.listeBien)
 
-a = Agence()
-a.inscriptionVendeur()
-print(a.vendeur)
-
-print(a.listeBien)
